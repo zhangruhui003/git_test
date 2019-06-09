345420a4eb4aa89ace0847e8c475cbfd4454866e
zhanruhui <2353174082@qq.cn>
2019-06-09 21:43:41 +0800

    提交Hellojava1

---
+++ git_test_project/src/com/itheima/HelloJava.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class HelloJava {
+
+
+    public static void main(String[] args) {
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+        System.out.println(1111111111);
+    }
+}
